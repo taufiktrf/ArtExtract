4f6dfcedb5189096d0da370bb0a5763bd8b8c930
PSY222 <ss26348@naver.com>
2024-09-07 14:50:28 +0000

    cleaned up comments, fixed readme structure

--- ArtExtract_Soyoung/README.md
+++ ArtExtract_Soyoung/README.md
@@ -3,16 +3,29 @@
 #### Project overview
 The ArtExtract project leverages machine learning to revolutionize art conservation by uncovering hidden paintings through multispectral imaging. By creating a comprehensive dataset of multispectral images of paintings, the project aims to develop an AI model capable of detecting hidden artworks behind the canvas. The project's innovative techniques could lead to significant discoveries in art history. Initially, the focus is on generating high-quality multispectral images from RGB images, addressing the challenges and strategic approaches needed to achieve this goal.
 
-#### Dataset
+#### Model Structure
+<!-- Model architecture image and explanation will be added -->
 
-| Dataset  | Info | Info |
+#### Dataset Info
+| Dataset  | Info | Link |
 | ------------- | ------------- |------------- |
-| CAVE  | 32 scenes with full spectral resolution reflectance data from 400nm to 700nm at 10nm intervals (31 bands total)  | [Link](https://www.cs.columbia.edu/CAVE/databases/multispectral/)  |
+| CAVE  | 32 scenes with full spectral resolution reflectance data from 400-700nm at 10nm intervals | [Link](https://www.cs.columbia.edu/CAVE/databases/multispectral/)  |
 | Harvard | 50 indoor and outdoor images, with 25 images featuring mixed illumination  | [Link](https://vision.seas.harvard.edu/hyperspec/d2x5g3/)  |
+| Multispectral Imaging (MSI) | 2 mock-up paintings with multispectral data at 360–1150 nm intervals| [Link](https://projects.ics.forth.gr/cvrl/msi/)  |
 
-[Download dataset here.](https://drive.google.com/drive/folders/1tW_GlgvikZlo6fHXr8Sjq1yrLTHw4tSz?usp=share_link)
+Among the multispectral dataset mixed with different objects, we used painting images as a validation dataset to verify its robust performance to align with our reserach objectives.
 
-[ Dataset Structure]
+| Data  | CAVE  | Harvard |MSI |Total  GT Image | Multispectral Masks|
+| ------------| ------------- |------------- |  ------------- |------------- |------------- |
+|Train|25|77|0|102|102*8|
+|Val|2|0|2|4|4*8|
+
+To overcome the limited training dataset, we created [patchify.py](utils/patchify.py), which divides a single image into 4 smaller patches. This process increases the size of the training dataset by a factor of 4, effectively augmenting the existing data and enhancing the model's training potential.
+
+
+You can find out about the train/val dataset used for the study [here.](https://drive.google.com/drive/folders/1tW_GlgvikZlo6fHXr8Sjq1yrLTHw4tSz?usp=share_link)
+
+#### Dataset Structure
  ```               
 ├── train 
 │   │
@@ -22,16 +35,28 @@ The ArtExtract project leverages machine learning to revolutionize art conservat
     ├── rgb_images  # Allocated painting image                       
     └── ms_masks  
 ```
+#### Results
+- Final results will be released by the final evaluation period.
+
+1. Quantitative Analysis
+| Model  | PSNR  | LPIPS |SSIM |
+| ------------| ------------- |------------- |  ------------- |
+| TBD  | TBD  | TBD |TBD |
+
+3. Qualitative Analysis
+
 #### Directories
 ```                
 ├── unets               # Collection of different UNets            
 │   ├── transBlocks     # Chunks of Transformer blocks          
 │   └── ..       
-├── utils               # For the data loading, eval metrics and visualization
+├── utils               # For the data loading, eval metrics and visualization, create patches
 ├── model.py            # Best performing model       
 ├── train.py            # Train, test code       
 └── trainModel.ipynb    # Example 
 ```
+Feel free to experiment with different model architectures within the `unets/` directory. 
+
 #### Implementation guidance
 
 1) Install the required packages
@@ -43,18 +68,7 @@ pip install -r requirements.txt
 <!-- Example -->
 python train.py --trainpath  '../train/' --valpath '../val/' -lr 0.02 -e 100
 ```
-#### Results
-- Final results will be released by the final evaluation period.
-
-1. Quantitative Analysis
-2. Qualitative Analysis
 
 All the citations for the referred papers are cited at the top of each code base.
 
 You can find out more about the project on the [blog]([https://medium.com/@soyoungpark.psy](https://medium.com/@soyoungpark.psy/beneath-the-canvas-discovering-hidden-art-with-ai-part1-gsoc-24-3dc499758120)) where I explained the step-by-step process of the project in detail.
-
-
-
-
-
->>>>>>> PRbranch
--- ArtExtract_Soyoung/train.py
+++ ArtExtract_Soyoung/train.py
@@ -1,19 +1,18 @@
 import os
 import torch
+import warnings
 import argparse
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
-from torch.cuda.amp import autocast, GradScaler
-from torchvision.models.vgg import VGG16_Weights
-import torchvision.models as models
+import matplotlib.pyplot as plt
 from PIL import Image
-
-from utils.metrics import MS_SSIMLoss, EvalMetrics
+from torchvision import transforms, models
 from utils.data import load_datasets
-from utils.vizImg import plot_images, viz_train
 from model import SimplyUNet
-import warnings
+from utils.vizImg import plot_images, viz_train
+from utils.metrics import MS_SSIMLoss, EvalMetrics
+from torchvision.models.vgg import VGG16_Weights
 
 warnings.filterwarnings("ignore")
 
@@ -21,151 +20,173 @@ class VGGFeatureExtractor(nn.Module):
     def __init__(self):
         super(VGGFeatureExtractor, self).__init__()
         vgg = models.vgg16(weights=VGG16_Weights.DEFAULT)
-        vgg.features[0] = nn.Conv2d(1, 64, kernel_size=3, padding=1)
+        vgg.features[0] = nn.Conv2d(8, 64, kernel_size=3, padding=1)
         self.features = vgg.features[:23] 
         
     def forward(self, x):
         return self.features(x)
 
-def train_test_model(model, train_path, val_path, optimizer, scheduler, device,epochs):
+def train_test_model(model, train_path, val_path, optimizer, scheduler, device, epochs):
     vgg_feature_extractor = VGGFeatureExtractor().to(device)
-    model.to(device)
     ms_ssim_loss = MS_SSIMLoss().to(device)
-    train_loader, val_loader = load_datasets(train_path, val_path)
-    best_loss = float('inf')
     metrics = EvalMetrics(vgg_feature_extractor).to(device)
-    losses, train_lpipses, val_lpipses = [], [], []
-    train_psnrs, val_psnrs = [], []
-    train_ssims, val_ssims = [], []
-    scaler = GradScaler()
+    train_loader, val_loader = load_datasets(train_path, val_path)
     accumulation_steps = 2
     
     best_val_lpips = float('inf')
     best_model_dir = "best_model"
     os.makedirs(best_model_dir, exist_ok=True)
     best_model_number = 0
+    model.to(device)
+    
+    # Storage for metrics
+    losses, train_lpipses, val_lpipses = [], [], []
+    train_psnrs, val_psnrs = [], []
+    train_ssims, val_ssims = [], []
 
     for epoch in range(epochs):
         model.train()
         epoch_psnr, epoch_lpips, epoch_ssim = 0.0, 0.0, 0.0
-        temp_cnt = 0
+        total_samples = 0
+        total_loss = 0.0
+        cnt = 0
         optimizer.zero_grad()
-
         for step, (images, masks) in enumerate(train_loader):
+            # Images: [B, 3, H, W], Masks: [B, 8, 1, H, W]
             images, masks = images.to(device), masks.to(device)
-            with autocast():  # Mixed precision
-                output = model(images)
-                loss = 0.0
-                for i in range(images.size(0)):
-                    for j in range(images.size(1)):
-                        output_image = output[i, j].unsqueeze(0)
-                        target_image = masks[i, j]
-                        temp_cnt += 1
-                        if epoch % 3 == 0 and temp_cnt < 3:
-                            plot_images(output_image, target_image, epoch, j)
-                        output_image = F.normalize(output_image, dim=1)
-                        target_image = F.normalize(output_image, dim=1)
-                        loss += ms_ssim_loss(output_image, target_image)
-                        psnr_value, lpips_value, ssim_value = metrics(output_image, target_image)
-                        epoch_psnr += psnr_value.item()
-                        epoch_lpips += lpips_value.item()
-                        epoch_ssim += ssim_value.item()
-                        del output_image, target_image
-                loss /= (images.size(0) * images.size(1))
-                loss /= accumulation_steps
-
-            scaler.scale(loss).backward()
+            output = model(images)  # Output: [B, 8, H, W]
+            masks = masks.squeeze(2)  # Remove the singleton dimension
+            assert masks.shape == output.shape  # [B, 8, H, W]
+            cnt += 1
+            # Check the results visually every N epochs (set to 3 now)
+            if epoch % 3 == 0 and cnt % 2 == 0: 
+                plot_images(output, masks, epoch)
+            # Normalize the entire batch
+            output = F.normalize(output, dim=1)
+            masks = F.normalize(masks, dim=1)
+                
+            # Calculate loss for the batch
+            loss = ms_ssim_loss(output, masks) / accumulation_steps
+            loss.backward()
 
             if (step + 1) % accumulation_steps == 0:
-                scaler.step(optimizer)
-                scaler.update()
+                optimizer.step()
                 scheduler.step()
                 optimizer.zero_grad()
+                
+            # Accumulate loss
+            total_loss += loss.item()
+            psnr_value, lpips_value, ssim_value = metrics(output, masks)
+            batch_size, num_channels = output.size(0), output.size(1)
+            total_samples += batch_size
+            
+            epoch_psnr += psnr_value.item() 
+            epoch_lpips += lpips_value.item() 
+            epoch_ssim += ssim_value.item() 
+            print(epoch_psnr,epoch_lpips,epoch_ssim)
+            torch.cuda.empty_cache()
 
-            losses.append(loss.item())
-            del loss
-            torch.cuda.empty_cache()  
-
-        NCT = len(train_loader) * images.size(0) * images.size(1)
-        epoch_lpips /= NCT
-        epoch_psnr /= NCT
-        epoch_ssim /= NCT
-
-        del images, masks
-        torch.cuda.empty_cache()  
-
+        num_batches = len(train_loader)        
+        average_loss = total_loss / num_batches
+        epoch_lpips /= num_batches
+        epoch_psnr /= num_batches
+        epoch_ssim /= num_batches
+        
+        losses.append(average_loss)
+        
         with torch.no_grad():
             train_lpipses.append(epoch_lpips)
             train_psnrs.append(epoch_psnr)
             train_ssims.append(epoch_ssim)
+            
+            # Evaluate the model on the validation set
             val_psnr, val_lpips, val_ssim = evaluate_model(model, val_loader, device, epoch, metrics)
             val_lpipses.append(val_lpips)
             val_psnrs.append(val_psnr)
             val_ssims.append(val_ssim)
             
-            # Save the best model based on validation LPIPS and delete previous best model
+            # Save the best model based on validation LPIPS
             if val_lpips < best_val_lpips:
                 best_val_lpips = val_lpips
                 new_model_path = os.path.join(best_model_dir, f"best_model_{best_model_number}_{best_val_lpips:.4f}.pth")
                 if best_model_number > 0:
-                    old_model_path = os.path.join(best_model_dir, f"best_model_{best_model_number - 1}_{best_val_lpips:.4f}.pth")
+                    old_model_path = os.path.join(best_model_dir, f"best_model_{best_val_lpips:.4f}.pth")
                     if os.path.exists(old_model_path):
                         os.remove(old_model_path)
                 torch.save(model.state_dict(), new_model_path)
                 best_model_number += 1
                 print(f"Best model saved with Validation LPIPS: {best_val_lpips:.4f}")
 
-
             print(f"Epoch [{epoch+1}/{epochs}] Train PSNR: {epoch_psnr:.4f}, Val PSNR: {val_psnr:.4f}, Train LPIPS: {epoch_lpips:.4f}, Val LPIPS: {val_lpips:.4f}, Train SSIM: {epoch_ssim:.4f}, Val SSIM: {val_ssim:.4f}")
 
-    print("Train/Test completed")
-    # Uncomment the below if you want to check the training progress
-    # viz_train(losses, train_lpipses, val_lpipses, train_psnrs, val_psnrs, train_ssims, val_ssims)
-    
-# Evaluate model with the validation dataset
-# Current Val dataset is limited (Multispectral dataset of paintings)
+    print("Training and validation completed.")
+    # Visualize training progress
+    viz_train(losses, train_lpipses, val_lpipses, train_psnrs, val_psnrs, train_ssims, val_ssims)
+
 def evaluate_model(model, val_loader, device, epoch, metrics):
     model.eval()
     val_psnr, val_lpips, val_ssim = 0.0, 0.0, 0.0
+    total_samples = 0
 
     with torch.no_grad():
         for images, masks in val_loader:
-            images, masks = images.to(device), masks.to(device)
-            output = model(images)
-            for i in range(images.size(0)):
-                for j in range(images.size(1)):
-                    output_image = output[i, j].unsqueeze(0)
-                    target_image = masks[i, j]
-                    output_image = F.normalize(output_image, dim=1)
-                    target_image = F.normalize(output_image, dim=1)
-                    psnr_value, lpips_value, ssim_value = metrics(output_image, target_image)
-                    val_psnr += psnr_value.item()
-                    val_lpips += lpips_value.item()
-                    val_ssim += ssim_value.item()
-                    del output_image, target_image
-
-    NCT = len(val_loader) * images.size(0) * images.size(1)
-    val_psnr /= NCT
-    val_lpips /= NCT
-    val_ssim /= NCT
+            images, masks = images.to(device), masks.to(device) 
+            batch_size, num_channels = images.size(0), images.size(1)
+            output = model(images)  
+            masks = masks.squeeze(2)  
+            assert masks.shape == output.shape
+            
+            # Normalize the entire batch
+            output = F.normalize(output, dim=1)
+            masks = F.normalize(masks, dim=1)
+                
+            # Calculate metrics for the batch
+            psnr_value, lpips_value, ssim_value = metrics(output, masks)
+            total_samples += batch_size 
+            val_psnr += psnr_value.item()
+            val_lpips += lpips_value.item()
+            val_ssim += ssim_value.item()
+            torch.cuda.empty_cache()
+    num_batches = len(val_loader)
+    val_psnr /= num_batches
+    val_lpips /= num_batches
+    val_ssim /= num_batches
     
-    del images, masks
-    torch.cuda.empty_cache()
     return val_psnr, val_lpips, val_ssim
 
 def gen_img(model, best_model_path, test_path, output_dir, device):
     model.load_state_dict(torch.load(best_model_path))
+    model.to(device)
     model.eval()
+    transform = transforms.Compose([
+        transforms.Resize((512, 512)),  
+        transforms.ToTensor(),   
+    ])
     
-    test_loader = load_datasets(train_path, test_path,False)
     os.makedirs(output_dir,exist_ok=True)
-    for i, (imgs,_) in enumerate(test_loader):
-        imgs = imgs.to(device)
-        output = model(imgs)
-        for j in range(output.size(1)):
-            output_channel = output[:, j, :, :]  # Extract jth channel from the output
-            img_name = str(output_dir).split('/')[-1]
-            save_image(output_channel, os.path.join(img_name, f"_channel_{j}.png"))
+    for img_name in os.listdir(test_path):
+        img_path = os.path.join(test_path, img_name)
+ 
+        if os.path.isfile(img_path):
+            # Open and transform the image
+            img = Image.open(img_path).convert('RGB')
+            img_tensor = transform(img).unsqueeze(0).to(device) 
+            with torch.no_grad():
+                output = model(img_tensor)
+
+            for j in range(output.size(1)): 
+                output_channel = output[:, j, :, :] 
+                output_channel_np = output_channel.cpu().detach().numpy()
+                
+                plt.figure(figsize=(5, 5))
+                plt.imshow(output_channel_np[0], cmap='gray')
+                plt.title(f'{os.path.splitext(img_name)[0]}_channel_{j}')
+                plt.axis('off')
+                
+                channel_img_name = f"{os.path.splitext(img_name)[0]}_channel_{j}.png"
+                plt.savefig(os.path.join(output_dir, channel_img_name), bbox_inches='tight', pad_inches=0)
+                plt.close()
+    print('Successfully saved image.')
 
 def get_args():
     parser = argparse.ArgumentParser(description="Train and evaluate a U-Net model")
